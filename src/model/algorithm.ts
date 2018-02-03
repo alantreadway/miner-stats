d34f738abee13029d8e92d7a68f502f45df23d48
Alan Treadway <alan@alantreadway.net>
2018-02-03 10:43:49 +0000

    Add algorithm metadata

--- src/model/algorithm.ts
+++ src/model/algorithm.ts
@@ -2,6 +2,8 @@ export type Algorithm =
   'sha256' |
   'ethash' |
   'equihash' |
+  'blake2s' |
+  'blakecoin' |
   'lyra2re2' |
   'lyra2z' |
   'neoscrypt' |
@@ -16,24 +18,55 @@ export type Algorithm =
   'scrypt' |
   'x11';
 
-const ALGORITHMS: Algorithm[] = [
-  'sha256',
-  'ethash',
-  'equihash',
-  'lyra2re2',
-  'lyra2z',
-  'neoscrypt',
-  'groestl',
-  'keccak',
-  'skein',
-  'cryptonight',
-  'myriad',
-  'yescrypt',
-  'qubit',
-  'sia',
-  'scrypt',
-  'x11',
-];
+const enum HashRate {
+  KH = 1e3,
+  GH = 1e9,
+  MH = 1e6,
+}
+
+export interface AlgorithmMetadata<T extends Algorithm> {
+  algorithm: T;
+  hashRateMultiplier: HashRate;
+}
+
+function buildAlgorithmMetadata<T extends Algorithm>(algo: T): AlgorithmMetadata<T> {
+  let hashRate = HashRate.MH;
+  if (algo === 'blake2s' || algo === 'blakecoin') {
+    hashRate = HashRate.GH;
+  } else if (algo === 'yesscript') {
+    hashRate = HashRate.KH;
+  }
+
+  return {
+    algorithm: algo,
+    hashRateMultiplier: hashRate,
+  };
+}
+
+type AlgorithmMetadataDictionary = { [algo in Algorithm]: AlgorithmMetadata<algo> };
+
+export const ALGORITHM_METADATA: AlgorithmMetadataDictionary = {
+  blake2s: buildAlgorithmMetadata('blake2s'),
+  blakecoin: buildAlgorithmMetadata('blakecoin'),
+  cryptonight: buildAlgorithmMetadata('cryptonight'),
+  equihash: buildAlgorithmMetadata('equihash'),
+  ethash: buildAlgorithmMetadata('ethash'),
+  groestl: buildAlgorithmMetadata('groestl'),
+  keccak: buildAlgorithmMetadata('keccak'),
+  lyra2re2: buildAlgorithmMetadata('lyra2re2'),
+  lyra2z: buildAlgorithmMetadata('lyra2z'),
+  myriad: buildAlgorithmMetadata('myriad'),
+  neoscrypt: buildAlgorithmMetadata('neoscrypt'),
+  qubit: buildAlgorithmMetadata('qubit'),
+  scrypt: buildAlgorithmMetadata('scrypt'),
+  sha256: buildAlgorithmMetadata('sha256'),
+  sia: buildAlgorithmMetadata('sia'),
+  skein: buildAlgorithmMetadata('skein'),
+  x11: buildAlgorithmMetadata('x11'),
+  yescrypt: buildAlgorithmMetadata('yescrypt'),
+};
+
+export const ALGORITHMS = Object.keys(ALGORITHM_METADATA) as Algorithm[];
 
 export function isAlgorithm(val: string): val is Algorithm {
   return ALGORITHMS.find((a) => a === val) != null;
